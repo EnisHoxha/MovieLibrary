60068d79ac8221048c8414871764d779d76ec0da
Enis Hoxha <68054720+EnisHoxha@users.noreply.github.com>
2022-12-05 13:41:22 +0100

    Update corsOptions.js

--- backend/corsOptions.js
+++ backend/corsOptions.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  origin: true,
+  origin: "http://164.92.184.155:5173",
 };
 
 module.exports = corsOptions;
