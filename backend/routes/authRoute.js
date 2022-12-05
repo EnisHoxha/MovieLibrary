d8eb653006b5f04d5406cabce9a7722ae55c779e
Enis Hoxha <68054720+EnisHoxha@users.noreply.github.com>
2022-12-05 15:49:15 +0100

    Update authRoute.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -4,8 +4,8 @@ const { login, register, logout } = require("../controllers/authController");
 const cors = require("cors");
 const corsOptions = require("../middlewares/corsMiddleware");
 
-router.post("/login", cors(corsOptions), login);
-router.post("/register", cors(corsOptions), register);
-router.post("/logout", cors(corsOptions), logout);
+router.post("/login",login);
+router.post("/register",register);
+router.post("/logout", logout);
 
 module.exports = router;
